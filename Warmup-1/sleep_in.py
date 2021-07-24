ab447774fbd26239f6bfbf85b5f038640285a715
YoussefAbdelwahed <youssef.mahmoud.behiry08@gmail.com>
2021-07-24 18:09:38 +0200

    Added warmup-1, sleep_1 problem solution

---
+++ Warmup-1/sleep_in.py
@@ -0,0 +1,30 @@
+'''
+About the problem:
+
+The parameter weekday is True if it is a weekday, and the parameter vacation is True
+if we are on vacation. We sleep in if it is not a weekday or we're on vacation.Return True if we sleep in.
+
+# Examples:
+sleep_in(False, False) → True
+sleep_in(True, False) → False
+sleep_in(False, True) → True
+'''
+
+def sleep_in_v1(weekday, vacation):
+    if not weekday or vacation:
+        return True
+    return False
+
+def sleep_in_v2(weekday, vacation):
+    return not weekday or vacation
+
+# Remember that the order of the booleans operators is the following: (not, and, or).
+# As an example: if passed Arguments were sleep_in(True, True):
+
+# The boolean expression (not weekday or vacation) will be evaluated in this order:
+#                               False or True
+#                                    True
+
+
+
+# The problem link: https://codingbat.com/prob/p173401
